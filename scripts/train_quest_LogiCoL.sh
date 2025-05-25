fd71043c4b058781fce4d2867afbd1371a05d715
yanzhen4 <yanzhen4@illinois.edu>
2025-05-24 22:49:38 -0400

    update scripts

--- scripts/train_quest_LogiCoL.sh
+++ scripts/train_quest_LogiCoL.sh
@@ -5,24 +5,21 @@ export HUGGINGFACE_HUB_CACHE=/shared/yanzhen4/models_cache
 export PYTHONPATH=$(pwd):PYTHONPATH
 export NCCL_P2P_DISABLE=1
 
-experiment_name="quest_LogiCoL"
-exclusion_loss_weight=0.1
-exclusion_loss_margin=0.1
-subset_loss_weight=0.2
-subset_loss_margin=0.2
-
+experiment_name="quest_SupCon"
 model_name="intfloat/e5-base-v2" # Also try GTR, GTE, and Contriever! 
+
+# The train batch size is set to 4, which is the average group size for the group strategy. 
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 python3 main.py \
     --train \
     --validate \
     --experiment_name $experiment_name \
     --model_name  $model_name \
-    --train_queries /shared/yanzhen4/LogiCoL/data/quest_train_withVarients.jsonl \
-    --val_queries /shared/yanzhen4/LogiCoL/data/quest_val_withVarients.jsonl \
-    --test_queries /shared/yanzhen4/LogiCoL/data/quest_test_withVarients.jsonl \
-    --documents /shared/yanzhen4/LogiCoL/data/quest_text_w_id_withVarients.jsonl \
-    --train_batch_size 1 \
+    --train_queries /shared/yanzhen4/LogiCoL/data/quest_train.jsonl \
+    --val_queries /shared/yanzhen4/LogiCoL/data/quest_val.jsonl \
+    --test_queries /shared/yanzhen4/LogiCoL/data/quest_test.jsonl \
+    --documents /shared/yanzhen4/LogiCoL/data/quest_text_w_id.jsonl \
+    --train_batch_size 4 \
     --gpus 4 \
     --num_workers 16 \
     --precision "fp16" \
@@ -33,10 +30,6 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 python3 main.py \
     --lr_scheduler constant \
     --project_name set-ir_release \
     --batch_strategy group \
-    --alpha 0.5 \
-    --exclusion_loss_weight $exclusion_loss_weight \
-    --exclusion_loss_margin $exclusion_loss_margin \
-    --subset_loss_weight $subset_loss_weight \
-    --subset_loss_margin $subset_loss_margin \
+    --alpha 1 \
     --output_dir /shared/yanzhen4/LogiCoL/output/trained_models \
     --cache_dir /shared/yanzhen4/LogiCoL/output/cache \
