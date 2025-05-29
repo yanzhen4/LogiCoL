b985d847310fb60e8f36b0003cc1bde2ddd46820
yanzhen4 <yanzhen4@illinois.edu>
2025-05-29 11:41:51 -0400

    update script

--- scripts/train_quest_SupCon.sh
+++ scripts/train_quest_SupCon.sh
@@ -33,3 +33,26 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 python3 main.py \
     --alpha 1 \
     --output_dir /shared/yanzhen4/LogiCoL/output/trained_models \
     --cache_dir /shared/yanzhen4/LogiCoL/output/cache \
+
+# CUDA_VISIBLE_DEVICES=4,5,6,7 python3 main.py \
+#     --train \
+#     --validate \
+#     --experiment_name $experiment_name \
+#     --model_name  $model_name \
+#     --train_queries /shared/yanzhen4/LogiCoL/data/quest_train.jsonl \
+#     --val_queries /shared/yanzhen4/LogiCoL/data/quest_val.jsonl \
+#     --test_queries /shared/yanzhen4/LogiCoL/data/quest_test.jsonl \
+#     --documents /shared/yanzhen4/LogiCoL/data/quest_text_w_id.jsonl \
+#     --train_batch_size 4 \
+#     --gpus 4 \
+#     --num_workers 16 \
+#     --precision "fp16" \
+#     --learning_rate 1e-5 \
+#     --max_seq_length 512 \
+#     --warmup_steps 25 \
+#     --num_epoch 10 \
+#     --lr_scheduler constant \
+#     --project_name set-ir_release \
+#     --batch_strategy random \
+#     --output_dir /shared/yanzhen4/LogiCoL/output/trained_models \
+#     --cache_dir /shared/yanzhen4/LogiCoL/output/cache \
